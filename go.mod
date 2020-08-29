995d5374c50b45582858aecf05e4e373782d8c1b
Tamal Saha <tamal@appscode.com>
2020-08-29 14:34:16 -0700

    added-all

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/tamalsaha/gcs-version-demo
 
 go 1.15
 
-require cloud.google.com/go/storage v1.11.0
+require (
+	cloud.google.com/go/storage v1.11.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.30.0
+)
--- main.go
+++ main.go
@@ -3,13 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
+	"google.golang.org/api/option"
 	"io/ioutil"
 	"log"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/googleapi"
+	sv1 "google.golang.org/api/storage/v1"
 )
 
 func main() {
+	bucketName := "gcs-version-demo"
+	filename := "license2.txt"
+
+	// Use oauth2.NoContext if there isn't a good context to pass in.
+	ctx := context.Background()
+	gcs, err := sv1.NewService(ctx,  option.WithScopes(sv1.DevstorageReadWriteScope))
+	if err != nil {
+		log.Fatal(err)
+	}
+	obj, err := gcs.Objects.Get(bucketName, filename).Do()
+	if e, ok := err.(*googleapi.Error); ok {
+		if e.Code == 409 {
+			log.Fatal(err)
+		}
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(obj.Metadata)
+}
+
+func main2() {
 	ctx := context.Background()
 
 	client, err := storage.NewClient(ctx)
@@ -18,8 +43,13 @@ func main() {
 	}
 	// Read the object1 from bucket.
 	bucketName := "gcs-version-demo"
-	filename := "license.txt"
+	filename := "license2.txt"
 	rc, err := client.Bucket(bucketName).Object(filename).NewReader(ctx)
+	if e, ok := err.(*googleapi.Error); ok {
+		if e.Code == 409 {
+			log.Fatal(err)
+		}
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,14 +60,14 @@ func main() {
 	}
 	fmt.Println(string(body))
 
-
-	w := client.Bucket(bucketName).Object(filename).NewWriter(ctx)
-	defer w.Close()
-	w.Write()
-	body, err := ioutil.ReadAll(rc)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(body))
+	//
+	//w := client.Bucket(bucketName).Object(filename).NewWriter(ctx)
+	//defer w.Close()
+	//w.Write()
+	//body, err := ioutil.ReadAll(rc)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(string(body))
 
 }
